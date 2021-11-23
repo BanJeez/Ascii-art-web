6f395d2bcce7c642e4e7c8a7e1e55531a4343706
Your Name <you@example.com>
2021-11-23 13:30:35 +0000

    part1.1

--- ascii-art-web.go
+++ ascii-art-web.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test1!")
-}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// checkes if user is useing the right path
+	if r.URL.Path != "/test" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	// check for verifying the type of the request
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
 
-func about_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test 2")
+	fmt.Fprintf(w, "Hello Test!")
 }
 
 func main() {
-	http.HandleFunc("/", index_handler)
-	http.HandleFunc("/about/", about_handler)
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/test", helloHandler) // the web path
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }

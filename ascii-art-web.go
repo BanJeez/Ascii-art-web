1677d6636686421e9c08cbb7f5b0298d4816cd8c
Your Name <you@example.com>
2021-11-29 21:46:00 +0000

    nearly done

--- ascii-art-web.go
+++ ascii-art-web.go
@@ -6,6 +6,19 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	banner := r.FormValue("banner")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Banner = %s\n", banner)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// checkes if user is useing the right path
 	if r.URL.Path != "/test" {
@@ -22,7 +35,10 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fileServer1 := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer1)
 	http.HandleFunc("/test", helloHandler) // the web path
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {

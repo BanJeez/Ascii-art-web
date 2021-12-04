3f4045fe68fe99d4c1bd8b8bffa4475e75bd6da3
pkdch <pau.darco@gmail.com>
2021-12-04 15:55:11 +0000

    Wip0

--- ascii-art-web.go
+++ ascii-art-web.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,26 +21,49 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Banner = %s\n", banner)
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	// checkes if user is useing the right path
-	if r.URL.Path != "/test" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
+func assciArtHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// going to integrate with formHandler
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// check for verifying the type of the request
 	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
+	tplPath := filepath.Join("static", "form.html")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("Parse Error: %v", err)
+		http.Error(w, "Error when Parsing", http.StatusInternalServerError)
+		return
+	}
+	tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Execute Error: %v", err)
+		http.Error(w, "Error when Executing", http.StatusInternalServerError)
+		return
+	}
+}
 
-	fmt.Fprintf(w, "Hello Test!")
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/ascii-art":
+		assciArtHandler(w, r)
+	default:
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	}
 }
 
 func main() {
-	fileServer1 := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer1)
-	http.HandleFunc("/test", helloHandler) // the web path
-	http.HandleFunc("/form", formHandler)
+
+	// fileServer1 := http.FileServer(http.Dir("./static"))
+	// http.Handle("/", fileServer1)
+	// http.HandleFunc("/test", helloHandler) // the web path
+	http.HandleFunc("/", pathHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {

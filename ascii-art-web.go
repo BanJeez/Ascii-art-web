41cf346d6a5240932ae7b2a17d23f2d0d6da773b
pkdch <pau.darco@gmail.com>
2021-12-06 23:54:45 +0000

    wip1, added headers

--- ascii-art-web.go
+++ ascii-art-web.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"path/filepath"
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,29 +20,36 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func assciArtHandler(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
 }
 
 // going to integrate with formHandler
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	// check for verifying the type of the request
 	if r.Method != "GET" {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
-	tplPath := filepath.Join("static", "form.html")
-	tpl, err := template.ParseFiles(tplPath)
-	if err != nil {
-		log.Printf("Parse Error: %v", err)
-		http.Error(w, "Error when Parsing", http.StatusInternalServerError)
-		return
-	}
-	tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("Execute Error: %v", err)
-		http.Error(w, "Error when Executing", http.StatusInternalServerError)
-		return
-	}
+	// fileServer1 := http.FileServer(http.Dir("./static"))
+	// http.Handle("/", fileServer1)
+
+	// tplPath := filepath.Join("static", "form.html")
+	// tpl, err := template.ParseFiles(tplPath)
+	// if err != nil {
+	// 	log.Printf("Parse Error: %v", err)
+	// 	http.Error(w, "Error when Parsing", http.StatusInternalServerError)
+	// 	return
+	// }
+	// tpl.Execute(w, nil)
+	// if err != nil {
+	// 	log.Printf("Execute Error: %v", err)
+	// 	http.Error(w, "Error when Executing", http.StatusInternalServerError)
+	// 	return
+	// }
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -59,13 +64,14 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// have to put these two into pathHandler/homeHandler
+	fileServer1 := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer1)
 
-	// fileServer1 := http.FileServer(http.Dir("./static"))
-	// http.Handle("/", fileServer1)
-	http.HandleFunc("/", pathHandler)
+	http.HandleFunc("/*", pathHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", http.HandlerFunc(pathHandler)); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
